2ed727719fb0a556068deeec4b1803a6bb429737
furqoncreative <akunkhusus42@gmail.com>
2020-04-11 17:20:29 +0700

    Day 1 - Watermelon

--- src/day1/WaterMelon.kt
+++ src/day1/WaterMelon.kt
@@ -1,9 +1,7 @@
 package day1
 
 object Watermelon {
-    fun divide(w: Int): Boolean {
-        return  (w in 3..100 && w % 2 == 0 )
-    }
+    fun divide(w: Int): Boolean = w in 3..100 && w % 2 == 0
 }
 
 
