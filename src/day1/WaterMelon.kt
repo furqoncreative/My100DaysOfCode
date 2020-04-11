ca1cd3d7bda3712f6b2eec1aef95779201838416
furqoncreative <akunkhusus42@gmail.com>
2020-04-11 16:58:46 +0700

    Day 1 - Watermelon

---
+++ src/day1/WaterMelon.kt
@@ -0,0 +1,10 @@
+package day1
+
+object Watermelon {
+    fun divide(w: Int): Boolean {
+        return  (w in 3..100 && w % 2 == 0 )
+    }
+}
+
+
+
---
+++ src/day1/WatermelonTest.kt
@@ -0,0 +1,23 @@
+package day1
+
+import org.junit.Assert.*
+import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class WatermelonTest {
+
+    @Test
+    fun `testing basic cases`() {
+        assertTrue(Watermelon.divide(4))
+        assertFalse(Watermelon.divide(2));
+        assertFalse(Watermelon.divide(5));
+        assertTrue(Watermelon.divide(88));
+        assertTrue(Watermelon.divide(100));
+        assertFalse(Watermelon.divide(67));
+        assertTrue(Watermelon.divide(90));
+        assertTrue(Watermelon.divide(10));
+        assertFalse(Watermelon.divide(99));
+        assertTrue(Watermelon.divide(32));
+    }
+}
