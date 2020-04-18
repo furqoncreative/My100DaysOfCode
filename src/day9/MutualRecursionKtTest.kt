783e0438d53de1cbf9ae7d945f12295c93478a00
furqoncreative <akunkhusus42@gmail.com>
2020-04-19 05:41:02 +0700

    Day 9 - Mutual Recursion

---
+++ src/day9/MutualRecursionKtTest.kt
@@ -0,0 +1,26 @@
+package day9
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MutualRecursionKtTest {
+
+    fun checkF(input : Int, shouldBe : Int) = assertEquals(f(input), shouldBe)
+    fun checkM(input : Int, shouldBe : Int) = assertEquals(m(input), shouldBe)
+
+    @Test
+    fun basicTests() {
+        checkF(0, 1)
+        checkF(5, 3)
+        checkF(10, 6)
+        checkF(15, 9)
+        checkF(25, 16)
+        checkM(0, 0)
+        checkM(5, 3)
+        checkM(10, 6)
+        checkM(15, 9)
+        checkM(25, 16)
+    }
+
+}
