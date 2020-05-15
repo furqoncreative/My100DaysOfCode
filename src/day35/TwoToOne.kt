aa9836a556ee0639ebca916766b3651b37ee64f9
furqoncreative <akunkhusus42@gmail.com>
2020-05-15 19:45:14 +0700

    Day 35 - Two to One

---
+++ src/day35/TwoToOne.kt
@@ -0,0 +1,3 @@
+package day35
+
+fun longest(s1:String, s2:String) = s1.plus(s2).toSortedSet().joinToString("")
---
+++ src/day35/TwoToOneKtTest.kt
@@ -0,0 +1,17 @@
+package day35
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TwoToOneKtTest {
+
+    @Test
+    fun test() {
+        println("longest Fixed Tests")
+        assertEquals("aehrsty", longest("aretheyhere", "yestheyarehere"))
+        assertEquals("abcdefghilnoprstu", longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
+        assertEquals("acefghilmnoprstuy", longest("inmanylanguages", "theresapairoffunctions"))
+
+    }
+}
